b1d31d47730833e199bae379e3e0e9b46fd2938d
Jack-Rais <giorgio.caridi908@gmail.com>
2024-12-24 15:08:15 +0100

    None

--- graphical.py
+++ graphical.py
@@ -51,10 +51,6 @@ class Root:
         root.geometry(f'{width}x{height}+{posx}+{posy}')
         root.update()
 
-    
-    ## Aggiungere 
-
-
     def run(self):
         self.root.mainloop()
 
--- graphics/mods.py
+++ graphics/mods.py
@@ -108,7 +108,6 @@ class RectangleMode(Mode):
             'edges': []
         }
         self.nodes.append(new_node)
-        self.on_node_add(new_node)
 
         self.rect_id = None
 
@@ -121,8 +120,7 @@ class RectangleMode(Mode):
 
 
     def __call__(self, canvas:tk.Canvas, 
-                       nodes:list | None = None,
-                       on_node:Callable | None = None):
+                       nodes:list | None = None):
         
         self.canvas = canvas
         self.nodes = [] if nodes is None else nodes
@@ -134,8 +132,6 @@ class RectangleMode(Mode):
         self.current_end_y = None
 
         self.rect_id = None
-
-        self.on_node_add = on_node if on_node else lambda x: x
 
         self.canvas.bind('<Button-1>', self.on_mouse_press)
         self.canvas.bind('<B1-Motion>', self.on_mouse_motion)
@@ -160,8 +156,6 @@ class CancelMode(Mode):
                 self.canvas.delete(rect_id)
                 self.nodes.remove(rect)
 
-                self.on_node_delete(rect)
-
                 break
 
     def unbind(self):
@@ -170,13 +164,10 @@ class CancelMode(Mode):
 
 
     def __call__(self, canvas:tk.Canvas,
-                       nodes:list[dict] | None = None,
-                       on_node:Callable | None = None):
+                       nodes:list[dict] | None = None):
         
         self.canvas = canvas
         self.nodes = nodes if nodes else []
-
-        self.on_node_delete = on_node if on_node else lambda x: x
 
         self.canvas.bind('<Button-1>', self.on_mouse_touch)
 
@@ -202,63 +193,133 @@ class OpenMode(Mode):
             return False
         
 
-        def remove_rect(node, new_color):
-
-            x1, y1, x2, y2 = self.canvas.coords(node['id'])
-
-            self.nodes.remove(node)
-            self.canvas.delete(node['id'])
-
-            rect_id = self.canvas.create_rectangle(
-                x1,
-                y1,
-                x2,
-                y2,
-                outline = new_color,
-                width = 2
-            )
-
-            self.nodes.append({
-                'id': rect_id,
-                'edges': node['edges'] 
-            })
-        
-
         if isinstance(node := is_in(event.x, event.y), dict):
             
             if self.last_node:
 
-                if self.last_node == node:
-
-                    remove_rect(node, 'blue')
+                if self.last_node['id'] == node['id']:
+                    
+                    self.canvas.itemconfig(node['id'], outline = 'blue')
                     self.last_node = None
 
                 else:
 
-                    ### Aggiungere il collegamento da un nodo all'altro
-                    ### la linea deve passare per dove si toccano a metà
+                    self.canvas.itemconfig(node['id'], outline = 'blue')
+                    self.canvas.itemconfig(self.last_node['id'], outline = 'blue')
 
-                    remove_rect(node, 'blue')
-                    remove_rect(self.last_node, 'blue')
+                    if self.last_node['id'] in [x[0] for x in node['edges']]:
 
-                    if self.last_node['id'] in node['edges']:
+                        id_line = [x[1] for x in node['edges'] if x[0] == self.last_node['id']][0]
 
+                        self.last_node['edges'].remove((node['id'], id_line))
+                        node['edges'].remove((self.last_node['id'], id_line))
 
+                        self.canvas.delete(id_line)
                     
                     else:
 
-                        self.last_node['edges'].append(node['id'])
-                        node['edges'].append(self.last_node['id'])
-                        
-                        self.draw_connection(self.last_node, node)
+                        id_edge = self.draw_connection(self.last_node, node)
+
+                        if id_edge:
+                            self.last_node['edges'].append((node['id'], id_edge))
+                            node['edges'].append((self.last_node['id'], id_edge))
+
+                    self.last_node = None
 
             else:
 
-                remove_rect(node, 'green')
-                self.last_node = self.nodes[-1]
+                self.canvas.itemconfig(node['id'], outline = 'green')
+                self.last_node = node
+
+    
+    def are_rectangles_adjacent(self, rect1, rect2):
+
+        def normalize(rect):
+            (x1, y1), (x2, y2) = rect
+            return (min(x1, x2), min(y1, y2)), (max(x1, x2), max(y1, y2))
+
+        (x1_min, y1_min), (x1_max, y1_max) = normalize(rect1)
+        (x2_min, y2_min), (x2_max, y2_max) = normalize(rect2)
+
+        contact_points = []
+        type_contact = None
+
+        if x1_max == x2_min or x1_min == x2_max:
+
+            overlap_min = max(y1_min, y2_min)
+            overlap_max = min(y1_max, y2_max)
+
+            type_contact = 'orizzontale'
+
+            if overlap_min <= overlap_max:
+                contact_points = [(x1_max, overlap_min), (x1_max, overlap_max)] if x1_max == x2_min \
+                    else [(x1_min, overlap_min), (x1_min, overlap_max)]
+
+        elif y1_max == y2_min or y1_min == y2_max:
+
+            overlap_min = max(x1_min, x2_min)
+            overlap_max = min(x1_max, x2_max)
+
+            type_contact = 'verticale'
+            
+            if overlap_min <= overlap_max:
+                contact_points = [(overlap_min, y1_max), (overlap_max, y1_max)] if y1_max == y2_min \
+                    else [(overlap_min, y1_min), (overlap_max, y1_min)]
+                
+        if contact_points[0][0] == contact_points[1][0] and \
+            contact_points[0][1] == contact_points[1][1]:
+
+            return False, []
+
+        return bool(contact_points), contact_points, type_contact
     
 
     def draw_connection(self, node1, node2):
+
+        x1_1, y1_1, x2_1, y2_1 = self.canvas.coords(node1['id'])
+        x1_2, y1_2, x2_2, y2_2 = self.canvas.coords(node2['id'])
+
+        contact, where, type_contact = self.are_rectangles_adjacent(
+            ((x1_1, y1_1), (x2_1, y2_1)),
+            ((x1_2, y1_2), (x2_2, y2_2))
+        )
+
+        if not contact:
+            return False
+        
+        (x1, y1), (x2, y2) = where
+        x_pos, y_pos = (x1 + x2) / 2, (y1 + y2) / 2
+
+        x_pos1, y_pos1 = (x1_1 + x2_1) / 2, (y1_1 + y2_1) / 2
+        x_pos2, y_pos2 = (x1_2 + x2_2) / 2, (y1_2 + y2_2) / 2
+
+        if type_contact == 'verticale':
+            
+            punti = [
+                x_pos1, y_pos1,
+                x_pos, y_pos1,
+                x_pos, y_pos2,
+                x_pos2, y_pos2
+            ]
+
+        else:
+
+            punti = [
+                x_pos1, y_pos1,
+                x_pos1, y_pos,
+                x_pos2, y_pos,
+                x_pos2, y_pos2
+            ]
+
+        id_line = self.canvas.create_line(
+            *punti,
+            width = 2,
+            fill = 'red'
+        )
+
+        return id_line
+        
+
 
 
 
@@ -268,12 +329,10 @@ class OpenMode(Mode):
 
 
     def __call__(self, canvas:tk.Canvas,
-                       nodes:list | None = None,
-                       on_node:Callable | None = None):
+                       nodes:list | None = None):
         
         self.canvas = canvas
         self.nodes = nodes if nodes else []
-        self.on_arch_creation = on_node
 
         self.last_node = None
 
