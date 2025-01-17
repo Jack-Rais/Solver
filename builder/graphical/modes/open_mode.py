cc82d35280ff36eb3aa2e1b5d787ccda9adce3e0
JackRais <giorgio.caridi908@gmail.com>
2025-01-17 16:24:42 +0100

    None

--- builder/graphical/modes/open_mode.py
+++ builder/graphical/modes/open_mode.py
@@ -51,13 +51,11 @@ class OpenMode(Mode):
         if self.last_node:
 
             if self.last_node['id'] == node['id']:
-                print('3', self.last_node['id'])
                 self.canvas.itemconfig(node['id'], outline = 'blue')
                 self.last_node = None
 
             else:
 
-                print('1', self.last_node['id'])
                 self.canvas.itemconfig(node['id'], outline = 'blue')
                 self.canvas.itemconfig(self.last_node['id'], outline = 'blue')
 
@@ -65,11 +63,8 @@ class OpenMode(Mode):
                 self.last_node = None
 
         else:
-            print('2', self.last_node)
             self.canvas.itemconfig(node['id'], outline='green')
             self.last_node = node
-
-        print('4', self.last_node)
 
 
     def handle_edge_creation(self, node):
@@ -90,17 +85,12 @@ class OpenMode(Mode):
 
                 # Attendi che l'utente inserisca la capacità
                 while self.last_capacity is None:
-                    print(self.last_capacity)
                     self.canvas.winfo_toplevel().update()
-
-                print('6', self.last_node)
 
                 self.last_node['edges'].append((node['id'], id_edge, self.last_capacity))
                 node['edges'].append((self.last_node['id'], id_edge, self.last_capacity))
 
                 self.last_capacity = None
-
-        print('5', self.last_node)
 
 
     def are_rectangles_adjacent(self, rect1, rect2):
